298b145f28e3d8de5ffd0468271ea03116104dc1
Hyerin Joo <candy980426@gmail.com>
2022-05-02 13:08:15 +0900

    ⚡️ 영화 장르별 분류

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,20 +3,41 @@ import Movie from "../components/Movie";
 
 function Home() {
   const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
+  const [adventure, setAdventure] = useState([]);
+  const [animation, setAnimation] = useState([]);
+  const [crime, setCrime] = useState([]);
+  const getAdventure = async () => {
     const json = await (
       await fetch(
         `https://yts.mx/api/v2/list_movies.json?sort_by=download_count&genre=adventure&limit=5`
       )
     ).json();
-    setMovies(json.data.movies);
+    setAdventure(json.data.movies);
+    setLoading(false);
+  };
+  const getAnimation = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?sort_by=download_count&genre=animation&limit=5`
+      )
+    ).json();
+    setAnimation(json.data.movies);
+    setLoading(false);
+  };
+  const getCrime = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?sort_by=download_count&genre=crime&limit=5`
+      )
+    ).json();
+    setCrime(json.data.movies);
     setLoading(false);
   };
   useEffect(() => {
-    getMovies();
+    getAdventure();
+    getAnimation();
+    getCrime();
   }, []);
-  console.log(movies);
   return (
     <div>
       {loading ? (
@@ -25,13 +46,37 @@ function Home() {
         <div>
           <h1>Movie Ranking</h1>
           <p>Based on download count</p>
-          {movies.map((movie) => (
+          <h2>Adventure</h2>
+          {adventure.map((adventure) => (
             <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              year={movie.year}
-              rating={movie.rating}
+              key={adventure.id}
+              id={adventure.id}
+              coverImg={adventure.medium_cover_image}
+              title={adventure.title}
+              year={adventure.year}
+              rating={adventure.rating}
+            />
+          ))}
+          <h2>Animation</h2>
+          {animation.map((animation) => (
+            <Movie
+              key={animation.id}
+              id={animation.id}
+              coverImg={animation.medium_cover_image}
+              title={animation.title}
+              year={animation.year}
+              rating={animation.rating}
+            />
+          ))}
+          <h2>Crime</h2>
+          {crime.map((crime) => (
+            <Movie
+              key={crime.id}
+              id={crime.id}
+              coverImg={crime.medium_cover_image}
+              title={crime.title}
+              year={crime.year}
+              rating={crime.rating}
             />
           ))}
         </div>
