5b40b5353291582ca24f743870eef89705f65ffe
Hyerin Joo <candy980426@gmail.com>
2022-05-02 14:35:09 +0900

    💄 CSS 초기화

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes>
         <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
